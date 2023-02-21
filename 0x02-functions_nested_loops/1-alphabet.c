b238da2ab65b7d8bdc463409b6b6e3f7b019b38d
Your Name <you@example.com>
2023-02-21 14:12:28 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * print_alphabet - function that prints the alphabet, lowercase
- *   *
- *    * Return: 0
- *     */
+ * print_alphabet - function that prints the alphabet, lowercase
+ *
+ * Return: 0
+ */
 void print_alphabet(void)
 {
 	char c = 'a';
